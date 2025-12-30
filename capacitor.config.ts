4c3b87d4d3cca56aa3289171f15fc13a7a2e4754
Astromic <aiastromic@gmail.com>
2025-12-30 02:33:38 -0600

    chore: setup capacitor for android build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.astromic.app',
+  appName: 'Astromic',
+  webDir: 'dist'
+};
+
+export default config;
